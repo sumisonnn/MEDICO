bec6cb10ec511a4bcf83e236bd1493dfdf98c784
superxd2222 <sumisonxd@gmail.com>
2025-06-11 10:32:30 +0545

    updated router for admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./LandingPage";
 import HomePage from "./HomePage";
 import Login from "./Login";
 import Signup from "./Signup";
+import AdminDashboard from "./AdminDashboard";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/homepage" element={<HomePage />} />
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
         
       </Routes>
     </Router>
