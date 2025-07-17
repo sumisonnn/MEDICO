ec1289929fb8d287c22bf0ebd168df5fd59558c6
superxd2222 <sumisonxd@gmail.com>
2025-07-17 23:13:53 +0545

    update

--- client/src/components/ProtectedRoute.tsx
+++
@@ -1,5 +0,0 @@
-import (Navigate) from 'react-router-dom';
-
-
-const ProtectedRoute = ({ children }) => { children: 35 X,Element }) => {
-  const isAuthenticated = 
--- client/src/index.js
+++
@@ -1,4 +0,0 @@
-const { connection } = require('./db'); // replace './db' with the path to your Sequelize config file
-
-// Call the connection function to test the database connection
-connection();
